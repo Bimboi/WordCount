cf813004e175d441b0939dfd6d91e39512ee3b44
Mark Caldoza <caldozamark222@gmail.com>
2021-05-15 12:54:24 +0800

    Can create subplots

--- WordCount.py
+++ WordCount.py
@@ -19,57 +19,146 @@ def count_words():
         if file[-3:] == "txt":
             txt = open(file)
             # read text file per enter line
-            for line in txt:
-                dict1 = get_dict(line)
+            dict1 = get_dict(txt)
 
             txt.close()
         elif file[-4:] == "docx":
             doc = docx.Document(file)
             full_text = []
             for para in doc.paragraphs:
-                # join paragraphs into one line in index 0 of full_text
+                # list paragraphs
                 full_text.append(para.text)
 
-            dict1 = get_dict(full_text[0])
+            dict1 = get_dict(full_text)
         else:
             raise FileNotFoundError
 
-        # sort in ascending order
-        sorted_dict1 = sorted(dict1.items())
-        # separate words for x-labels and their count for y-labels
-        sorted_words = list(item[0] for item in sorted_dict1)
-        words_count = list(item[1] for item in sorted_dict1)
-
-        print(sorted_dict1)
-
-        plt.figure(figsize=(22, 8))
-        plt.bar(sorted_words, words_count)
-        plt.xlabel("Words")
-        plt.ylabel("Word Count")
-        plt.title("Number of times a word is used")
-
-        plt.xticks(rotation=90)
-        plt.grid()
-        plt.tight_layout()
-        plt.savefig('wc_figure.png', bbox_inches='tight', dpi=350)
+        length = len(dict1)
+        print("Unique word count: " + str(length))
+        if 201 > length > 0:
+            get_figures(dict1, length)
+        elif 201 < length:
+            print("\nNumber of unique words exceed: " + str(length))
+        else:
+            print("\nFile does not contain a word!")
 
     except FileNotFoundError:
         print("File not found")
+    except Exception:
+        print("Unable to open file")
 
 
 def get_dict(words):
     temp_dict = dict()
-    # for each enter line, split it to have a set of words
+    # for each string, split it to have a set of words
     # for each word, remove special characters, start and end spaces, then format to lowercase
-    alpha_num_list = filter(str.strip, [re.sub('[^a-zA-Z]+', '', _).lower() for _ in words.split()])
-    for word in alpha_num_list:
-        if word in temp_dict:
-            temp_dict[word] = temp_dict[word] + 1
-        else:
-            # first time counting the word
-            temp_dict[word] = 1
-    print(temp_dict)
+    for line in words:
+        alpha_num_list = filter(str.strip, [re.sub('[^a-zA-Z]+', '', _).lower() for _ in line.split()])
+        for word in alpha_num_list:
+            if word in temp_dict:
+                temp_dict[word] = temp_dict[word] + 1
+            else:
+                # first time counting the word
+                temp_dict[word] = 1
     return temp_dict
+
+
+def get_figures(dict1, length):
+    # sort in ascending order
+    sorted_dict1 = sorted(dict1.items())
+    # separate words for x-labels and their count for y-labels
+    sorted_words = list(item[0] for item in sorted_dict1)
+    words_count = list(item[1] for item in sorted_dict1)
+
+    if 201 > length > 150:
+        # round down
+        one_fourth_index = length // 4
+        half_index = one_fourth_index * 2
+        three_fourth_index = one_fourth_index * 3
+
+        # split both lists into four
+        first_sorted = sorted_words[:one_fourth_index]
+        second_sorted = sorted_words[one_fourth_index:half_index]
+        third_sorted = sorted_words[half_index:three_fourth_index]
+        fourth_sorted = sorted_words[three_fourth_index:]
+        first_count = words_count[:one_fourth_index]
+        second_count = words_count[one_fourth_index:half_index]
+        third_count = words_count[half_index:three_fourth_index]
+        fourth_count = words_count[three_fourth_index:]
+
+        # first figure
+        get_two_figures(first_sorted, second_sorted, first_count, second_count)
+        plt.savefig('wc_figure1.png', bbox_inches='tight', dpi=250)
+
+        # second figure
+        get_two_figures(third_sorted, fourth_sorted, third_count, fourth_count)
+        plt.savefig('wc_figure2.png', bbox_inches='tight', dpi=250)
+
+    elif 150 > length > 100:
+        # round down
+        one_third_index = length // 3
+        two_third_index = one_third_index * 2
+
+        # split both lists into three
+        first_sorted = sorted_words[:one_third_index]
+        second_sorted = sorted_words[one_third_index:two_third_index]
+        third_sorted = sorted_words[two_third_index:]
+        first_count = words_count[:one_third_index]
+        second_count = words_count[one_third_index:two_third_index]
+        third_count = words_count[two_third_index:]
+
+        # first figure
+        get_two_figures(first_sorted, second_sorted, first_count, second_count)
+        plt.savefig('wc_figure1.png', bbox_inches='tight', dpi=250)
+
+        # second figure
+        get_one_figure(third_sorted, third_count)
+        plt.savefig('wc_figure2.png', bbox_inches='tight', dpi=250)
+
+    elif length > 50:
+        # round down
+        middle_index = length // 2
+
+        # split both lists into two
+        first_sorted = sorted_words[:middle_index]
+        second_sorted = sorted_words[middle_index:]
+        first_count = words_count[:middle_index]
+        second_count = words_count[middle_index:]
+
+        get_two_figures(first_sorted, second_sorted, first_count, second_count)
+        plt.savefig('wc_figure.png', bbox_inches='tight', dpi=250)
+
+    else:
+        get_one_figure(sorted_words, words_count)
+        plt.savefig('wc_figure.png', bbox_inches='tight', dpi=250)
+
+
+def get_one_figure(sorted1, count1):
+    # figsize=(width, height)
+    # barh - plot horizontal bar
+    # grid(axis='x') - add only vertical lines of a grid
+    plt.figure(figsize=(11, 9))
+    plt.barh(sorted1, count1)
+    plt.title("Number of times a word is used")
+    plt.xlabel("Word count")
+    plt.ylabel("Words in the file")
+    plt.grid(axis='x')
+
+
+def get_two_figures(sorted1, sorted2, count1, count2):
+    fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(18, 9))
+    ax1.barh(sorted1, count1)
+    ax2.barh(sorted2, count2)
+
+    ax1.grid(axis='x')
+    ax2.grid(axis='x')
+    # main title
+    fig.suptitle("Number of times a word is used")
+    # common y-label
+    fig.text(0.5, 0, 'Word count', ha='center')
+    # common x-label
+    fig.text(0, 0.5, 'Words in the file', va='center', rotation='vertical')
+    plt.tight_layout(pad=1.0)
 
 
 def again():
